0380dd6be0c03b6af2beca72780a9afbf55c7888
Raphael BOUCHARD <rbouchard@hotmail.fr>
2022-05-19 14:43:38 +0200

    Ona nettoyé le code des cout parasites et des commentaires liés à ça dans les fichiers io.h io.cc et mpm_bas.h et .tcc

--- src/io/io.cc
+++ src/io/io.cc
@@ -182,13 +182,10 @@ std::string mpm::IO::output_folder() const {
 void mpm::IO::copy_input_file()
 {
 
-  //liste_nom_input_file_.push_back();
-  //std::cout << "ligne 184 io.cc liste des noms "<< liste_nom_input_file_[0] << '\n';
-  //std::cout << "working_dir_ = "<< working_dir_ << '\n';
   int longueur_chaine_working_directory = working_dir_.length();
-  //std::cout << "longueur_chaine_working_directory = " << longueur_chaine_working_directory << '\n';
   int longueur_chaine_chemin_absolu;
   std::string convert_file;
+  
    for (const auto & file : boost::filesystem::directory_iterator(working_dir_) )
        {
          // on convertie l'objet boost::filesystem en string pour le manipuler
@@ -204,11 +201,7 @@ void mpm::IO::copy_input_file()
          // a partir de laquelle le working_dir_ commence.
          longueur_chaine_chemin_absolu= convert_file.rfind(working_dir_);
          longueur_chaine_chemin_absolu += longueur_chaine_working_directory;
-         /*std::cout << "longueur_chaine_chemin_absolu = "<< longueur_chaine_chemin_absolu << '\n';
 
-         std::cout << "convert_file[0] = " << convert_file[1] << '\n';
-         std::cout << "convertfile [ ] = "<< convert_file[longueur_chaine_chemin_absolu + longueur_chaine_working_directory] << '\n';
-         std::cout << "convertfile [ ] = "<< convert_file[48] << '\n';*/
          // on surppime ainsi le debuit de la chain de caraère (le chemin absolu )
          // on obtient ainsi juste le nom du fichier
          //std::cout <<  longueur_chaine_chemin_absolu + longueur_chaine_working_directory<< '\n';
@@ -218,9 +211,8 @@ void mpm::IO::copy_input_file()
          // on ne conserve que les fichiers (ie : les noms avec un . dedans)
          if(convert_file.find(".") != std::string::npos)
          {
-           //std::cout << "convert_file = "<< convert_file << '\n';
-           liste_nom_input_file_.push_back(convert_file);
-           //std::cout << "chemin_copy_input_ = " << chemin_copy_input_ << '\n';
+
+           //liste_nom_input_file_.push_back(convert_file);
 
            // copy les fichiers dans le repertoire prevu pour
            boost::filesystem::copy_file(working_dir_+convert_file, chemin_copy_input_+convert_file,boost::filesystem::copy_option::overwrite_if_exists);
